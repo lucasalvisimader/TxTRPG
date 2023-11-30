856987542c7b4f1c26050a1b0dcacf34e20193be
Lucas <105462862+lucasalvisimader@users.noreply.github.com>
2023-11-29 21:58:57 -0300

    changed json, bugs to fix

--- src/pages/chapterOne/ChapterOne.js
+++ src/pages/chapterOne/ChapterOne.js
@@ -13,20 +13,20 @@ import json from '../../jsons/ChapterOne.json';
 export const ChapterOne = () => {
     const [choices, setChoices] = useState([]);
     const [whichPartOfChapter, setWhichPartOfChapter] = useState(0);
+    const [chapter, setChapter] = useState();
 
     const getChoices = () => {
-        json.chapter_one.forEach(jsonFormatted => {
-            jsonFormatted[0].forEach(choice => {
-                if (choice[0] === "choice") {
-                    setChoices((prevState) => {
-                        const key = choice[1];
-                        if (!prevState.some(item => item[0] === key)) {
-                            return [...prevState, [key, choice[2]]];
-                        }
-                        return prevState;
-                    });
-                }
-            })
+        json.forEach(choice => {
+            console.log(choice)
+            if (choice[0] === "choice") {
+                setChoices((prevState) => {
+                    const key = choice[1];
+                    if (!prevState.some(item => item[0] === key)) {
+                        return [...prevState, [key, choice[2]]];
+                    }
+                    return prevState;
+                });
+            }
         })
     }
 
@@ -34,9 +34,14 @@ export const ChapterOne = () => {
         getChoices();
     }, [])
 
+    useEffect(() => {
+        setChapter(json.chapter_one)
+        setWhichPartOfChapter("0");
+    }, [])
+
     return (<>
         <div className='chapter_one_container'>
-            <DialogText title={json.chapter_one[0][0][0][1]} text={json.chapter_one[0][0][1][1]} choices={choices}
+            <DialogText title={json.chapter_one[0][0][1]} text={json.chapter_one[0][1][1]} choices={choices}
                 whichPartOfChapter={whichPartOfChapter} setWhichPartOfChapter={setWhichPartOfChapter} />
         </div>
     </>);
